6bb5695a57da1e0b6f27750e10c455457c03517d
Lapi <lapi@lapi-linux.(none)>
2011-10-28 22:41:01 +0200

    Inital module

---
+++ entity_reference_tree.api.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Retrieve a tree of an entity hierarchy
+ *
+ * @param $element
+ *   The main checkbox_tree element.
+ * @param $allowed_ids
+ *   A list of allowed Ids
+ * @param $label
+ *   A list of allowed Ids
+ *
+ * @return
+ *   A nested tree of entities
+ */
+function hook_get_entity_hierarchy($element, $allowed_ids, $label = '') {
+}
